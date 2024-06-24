7862f600ad03e2b749f65f866f90579a49f560a5
Ramsha06 <164333955+Ramsha06@users.noreply.github.com>
2024-06-24 20:23:52 +0500

    Add files via upload

---
+++ 100 days coding challenge/D9/Q27/main.ts
@@ -0,0 +1,60 @@
+// Q27 - Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+
+// If the alien is green, print a message that the player earned 5 points.
+
+// If the alien is yellow, print a message that the player earned 10 points.
+
+// If the alien is red, print a message that the player earned 15 points.
+
+// Write three versions of this program, making sure each message is printed for the appropriate color alien.
+
+let Alien_color: string= "green";
+if (Alien_color == "green")
+    {
+        console.log(" Player earned 5 points! ");
+
+    }
+
+else if (Alien_color == "yellow")
+        {
+            console.log("player earned 10 points.");
+        }
+        else if(Alien_color == "red")
+        {
+            console.log("player earned 15 points.");
+        }
+
+
+        // Write three versions of this program, making sure each message is printed for the appropriate color alien.
+
+        let Alien_colr: string= "yellow";
+        if (Alien_colr == "green")
+      {
+        console.log(" Player earned 5 points! ");
+
+      }
+
+        else if (Alien_colr == "yellow")
+        {
+            console.log("player earned 10 points.");
+        }
+        else if(Alien_colr == "red")
+        {
+            console.log("player earned 15 points.");
+        }
+
+
+        let Alien_colr1: string= "red";
+         if (Alien_colr1 == "green")
+        {
+        console.log(" Player earned 5 points! ");
+        }
+
+        else if (Alien_colr1== "yellow")
+        {
+            console.log("player earned 10 points.");
+        }
+        else if(Alien_colr1 == "red")
+        {
+            console.log("player earned 15 points.");
+        }
