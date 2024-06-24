f14a6481fcddf329f22a1d30a4ce6a73ebcaf6bd
Ramsha06 <164333955+Ramsha06@users.noreply.github.com>
2024-06-24 19:52:20 +0500

    Add files via upload

---
+++ 100 days coding challenge/D5/Q13/main.ts
@@ -0,0 +1,39 @@
+// // Q 13
+
+//ANOTHER PROGRAM
+
+let transportation: string[] = ["motorcycle", "car", "bicycle", "scooter", "electric skateboard"];
+
+// Print statements about favorite modes of transportation
+console.log("My Favorite Modes of Transportation:");
+
+transportation.forEach((mode) => {
+  console.log(`I would like to own a ${mode}.`);
+});
+
+
+//--------------------------------------------------------------------------------
+
+
+// const fav_Transport:string[]=["car","motorcycle","bicycle","boat","train"];
+
+// fav_Transport.forEach((mode)=>{
+//  switch(mode){
+//   case "motorcycle":
+//     console.log("This is motorcycle would like to own motorcycle");
+//     case "car":
+//       console.log("I dream of driving the car");
+//       case "bicycle":
+//         console.log("I enjoy riding a vintage motorbike");
+//         case "boat":
+//           console.log("I love sailing on a yatch");
+//           case "train":
+//             console.log("I prefer travelling by high-speed train");
+//             default:
+//               console.log("i like various mode of transportaion");
+//               break;
+//   };
+// });
+
+
+
