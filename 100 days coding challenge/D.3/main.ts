2939d73e54b83fa18a4df1cc9eba45b0db19133c
Ramsha06 <164333955+Ramsha06@users.noreply.github.com>
2024-06-24 19:48:28 +0500

    Add files via upload

---
+++ 100 days coding challenge/D.3/main.ts
@@ -0,0 +1,29 @@
+//Q7 - Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the 
+//number 8. Be sure to enclose your operations in print statements to see the results.
+
+console.log(5 + 3); // Addition
+console.log(16 / 2); // Division
+console.log(10 - 2); // Subtraction
+console.log(4 * 2); // Multiplication
+
+
+//Q8 - You should create four lines that look like this: console.log(5 + 3) Your output should simply be four lines 
+//with the number 8 appearing once on each line.
+
+console.log(5+3); //Addition
+console.log(5+3); //Addition
+console.log(5+3); //Addition
+console.log(5+3); //Addition
+
+//Q9 - Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that
+// reveals your favorite number. Print that message.
+
+                   //      Ramsha   8/3/2024
+
+                   let fav_Number:number=7;
+                   //let favourite_Nmuber:string="your favourite number is";
+                   //console.log(fav_Number);    // print favourite number which is 7
+                   console.log(`your favourite number is , ${fav_Number}`);  // will print message (your favourite number is) and fav_number (7)
+
+
+
