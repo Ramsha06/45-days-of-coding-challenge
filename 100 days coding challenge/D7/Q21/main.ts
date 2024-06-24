5751971d28b64f507d39a1d6249e5c40ac0c2638
Ramsha06 <164333955+Ramsha06@users.noreply.github.com>
2024-06-24 20:03:24 +0500

    Add files via upload

---
+++ 100 days coding challenge/D7/Q21/main.ts
@@ -0,0 +1,18 @@
+//Q21 - They think of something you could store in a TypeScript Object.
+// Write a program that creates Objects containing these items.
+
+let Employe :  {
+    id:number;
+    Name: String;
+    company: string;
+    Designation:string;
+}={
+    id:7,
+    Name: "Ramsha",
+    company: "ICI",
+    Designation:"Manager",
+
+}
+
+console.log(`check this employe details,  her id: ${Employe.id}, her Name :${Employe.Name}. her Company: ${Employe.company}, her Designation: ${Employe.Designation}`);
+
