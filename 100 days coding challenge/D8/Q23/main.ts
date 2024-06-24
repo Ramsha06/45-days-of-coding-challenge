21bdb5a7b0219cfbedf7d94d4a6d1a2d66f1a654
Ramsha06 <164333955+Ramsha06@users.noreply.github.com>
2024-06-24 20:16:23 +0500

    Add files via upload

---
+++ 100 days coding challenge/D8/Q23/main.ts
@@ -0,0 +1,37 @@
+//Q23 - Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your
+// prediction for the results of each test. Your code should look something like this:
+
+let car = "Mercedes-Benz";
+
+console.log( `car == "Mercedes-Benz"? I predict True`);
+console.log(car == "Mercedes-Benz");
+
+console.log( `car == "Range Rover"? I predict false`);
+console.log(car == "Range Rover");
+
+console.log( `car == "Fortuner"? I predict false`);
+console.log(car == "Fortuner");
+
+console.log( `car == "Parado"? I predict false`);
+console.log(car == "Parado");
+
+console.log( `car == "GLI"? I predict false`);
+console.log(car == "GLI");
+
+console.log( `car == "Swift"? I predict false`);
+console.log(car == "false");
+
+console.log( `car !== "PARADO"? I predict true`);
+console.log(car !== "PARADO");
+
+console.log( `car !== "KIA"? I predict True`);
+console.log(car !== "KIA");
+
+console.log( `car !== "sports car"? I predict True`);
+console.log(car !== "sports car");
+
+console.log( `car !== "civic"? I predict True`);
+console.log(car !== "civic");
+
+console.log( `car !== "sportage"? I predict True`);
+console.log(car !== "sportage");
