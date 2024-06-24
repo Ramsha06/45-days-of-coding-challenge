7c75bed21f012bd08c9fd0a502687e984c93fea9
Ramsha06 <164333955+Ramsha06@users.noreply.github.com>
2024-06-24 19:54:02 +0500

    Add files via upload

---
+++ 100 days coding challenge/D5/Q14/guest.ts
@@ -0,0 +1,20 @@
+let guests:string[]=["sifra","sofia","Dany"];
+guests.forEach((guest)=>{
+console.log(` Hi ${guest} would you like to join me in dinner today?`);
+});
+
+    //Q15
+
+let cantmake= "Dany";
+console.log(`${cantmake} can't make to the dinner today..`);
+
+//new guest
+let newguest = "Dazil";
+guests[guests.indexOf(cantmake)]=newguest;
+
+//invitation
+
+guests.forEach((guest)=>
+{
+    console.log(` ${guest} would like to come to the dinner today?`);
+});
