18f7948e9b5139cc52455ff373808665ad44678a
Ramsha06 <164333955+Ramsha06@users.noreply.github.com>
2024-06-24 20:22:56 +0500

    Add files via upload

---
+++ 100 days coding challenge/D9/Q26/main.ts
@@ -0,0 +1,37 @@
+// Q26 - Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
+
+// If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
+
+// If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+
+// Write one version of this program that runs the if block and another that runs the else block.
+
+
+
+
+let alien_color = "green";
+
+if (alien_color=="green")
+    {
+console.log("you have earned 5 points for shooting an alien !"); 
+    }
+
+else
+    {
+        console.log("You have earned 10 points!");
+    }
+
+ // Write one version of this program that runs the if block and another that runs the else block.
+
+ 
+let alien_colr = "red";
+
+if (alien_colr=="green")
+    {
+console.log("you have earned 5 points for shooting an alien !"); 
+    }
+
+   else 
+    {
+        console.log("You have earned 10 points!");
+    }
