e12d088a9746bac291ec0c59a6d3a9a9ea6b24a8
Ramsha06 <164333955+Ramsha06@users.noreply.github.com>
2024-06-24 19:46:04 +0500

    Add files via upload

--- 100 days coding challenge/CLASS array/main.ts
+++ 100 days coding challenge/CLASS array/main.ts
@@ -1,1 +1,14 @@
+let myUsers: string[]= ["Sifra","Sofia","Albert","Einstien"," ","Bill","Gates"];
 
+myUsers.push("Denim");
+console.log(myUsers);
+myUsers.pop();
+myUsers.pop();
+myUsers.push("linda");
+myUsers.shift();
+myUsers.unshift("R");
+console.log(myUsers);
+for(let i =0; i <= myUsers.length; i ++)
+    {
+        console.log(myUsers[i]);
+    }
