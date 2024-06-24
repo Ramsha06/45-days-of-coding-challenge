692412c1b67d099672d1227d4e5649dbf436f20d
Ramsha06 <164333955+Ramsha06@users.noreply.github.com>
2024-06-24 19:59:50 +0500

    Add files via upload

---
+++ 100 days coding challenge/D7/Q19/main.ts
@@ -0,0 +1,22 @@
+//Q19 - Dinner Guests: Working with one of the programs from Exercises 14 through 18,
+// print a message indicating the number of people you are inviting to dinner.
+
+let Dinner : string[]=["Herry Potter","Bill gates","susane","Einstien"];
+console.log(` ${Dinner.length} guests ,are coming to our wonderful dinner tomorrow`);
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+let guests: string[] = ["Albert Einstein", "Marie Curie", "Leonardo da Vinci", "Isaac Newton", "Charles Darwin"];
+console.log(`I am inviting ${guests.length} people to dinner.`);
+
+    
+/////////////////////////////////////////////////////////////////////////////////////
+
+let feast:string[]=["Albert Einstein", "Marie Curie", "Leonardo da Vinci", "Isaac Newton", "Charles Darwin"];
+
+for(let i=0;i<=feast.length;i++)
+       {
+              console.log(feast[i]);
+       }
+       
