ce96d79798c6db5f427c5d4c804f1aee0a767829
Ramsha06 <164333955+Ramsha06@users.noreply.github.com>
2024-06-26 11:41:34 +0500

    Add files via upload

---
+++ 100 days coding challenge/D17/Q50/main.ts
@@ -0,0 +1,10 @@
+//Q50 - Multiline Template Literals: Use template literals to create a multiline string that describes your ideal day. 
+//Include at least three different activities.
+
+let myIdealDay = `My ideal day would involve:
+1. Waking up early and going for a jog.
+2. Spending a few hours coding on a personal project.
+3. Ending the day by reading a good book.`;
+
+// Logging the multiline string to the console
+console.log(myIdealDay);
