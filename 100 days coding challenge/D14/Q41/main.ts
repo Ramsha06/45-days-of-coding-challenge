f7581f9e22dee943fb5741f10f2cb3ff3565ef98
Ramsha06 <164333955+Ramsha06@users.noreply.github.com>
2024-06-26 11:26:41 +0500

    Add files via upload

---
+++ 100 days coding challenge/D14/Q41/main.ts
@@ -0,0 +1,16 @@
+//Q41 - Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(),
+// which prints the name of each magician in the array.
+
+let Magicians:string[]=["yoyo","joker","chika"];
+
+function show_magicians(Magicians:string[])
+{
+    Magicians.forEach((Magicians)=>{
+        console.log(Magicians);
+
+    }
+        
+    );
+}
+
+show_magicians(Magicians);
