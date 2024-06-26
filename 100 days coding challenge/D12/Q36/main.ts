4b882fdd005889cf0d6d5d8a3d490536890332e3
Ramsha06 <164333955+Ramsha06@users.noreply.github.com>
2024-06-26 11:19:58 +0500

    Add files via upload

---
+++ 100 days coding challenge/D12/Q36/main.ts
@@ -0,0 +1,10 @@
+// Q36 - T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed
+//  on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. 
+//  Call the function.
+
+function make_Tshirt(size:string,message:string)
+{
+    console.log(`Making a ${size} shirt with a message ${message} printed on it.`);
+}
+
+make_Tshirt("'Medium size'", " 'We will won'");
