efe74ae296314f63875cc1092df289e8995297a7
Ramsha06 <164333955+Ramsha06@users.noreply.github.com>
2024-06-26 19:06:07 +0500

    Add files via upload

---
+++ 100 days coding challenge/D25/Q75/main.ts
@@ -0,0 +1,19 @@
+//Question 75: Compound Assignment Operators: Use compound assignment operators to simplify arithmetic operations. 
+//Start with x = 4 and perform a series of operations (addition, subtraction, multiplication, division) on x using
+// compound operators.
+
+function usecompoundoperators()
+{
+    let a = 1;
+    console.log("a befor a = ", a);
+    a += 1;
+    console.log("after addition a =" , a);
+    a -= 1;
+    console.log("after subtraction a =" , a);
+    a *= 1;
+    console.log("after multiplication a =" , a);
+    a /= 1;
+    console.log("after devision a =" , a);
+    
+}
+usecompoundoperators();
