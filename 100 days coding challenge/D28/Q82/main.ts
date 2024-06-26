1b96536b305b2eb4bef2e4e65a05a03fc0483bda
Ramsha06 <164333955+Ramsha06@users.noreply.github.com>
2024-06-26 19:19:16 +0500

    Add files via upload

---
+++ 100 days coding challenge/D28/Q82/main.ts
@@ -0,0 +1,10 @@
+//Question 82: Find the Length of a String: Write a function that takes a string as an argument and returns the number
+// of characters in the string.
+
+function stringlength(string :string):number
+{
+    return string.length;
+}
+
+console.log(stringlength("Sifra")); 
+    
