85eb3680a3d1b8d6fc06aa9b3aaf010e50b22895
Ramsha06 <164333955+Ramsha06@users.noreply.github.com>
2024-06-26 19:13:54 +0500

    Add files via upload

---
+++ 100 days coding challenge/D27/Q79/main.ts
@@ -0,0 +1,17 @@
+//Question 79: Creating and Accessing Object Properties: Construct an object representing a car with properties for make,
+// model, and year. Then, show how you can access the model property of the car.
+
+
+
+let car = {
+    make : "Prado",
+    model : "land_cruiser ",
+    year : 2024,
+
+};
+console.log(car);
+console.log(car.make);
+console.log(car.model);
+console.log(car.year);
+
+
