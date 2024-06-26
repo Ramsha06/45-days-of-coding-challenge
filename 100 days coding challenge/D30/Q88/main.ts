dc4bd63867626a265ed8b5eccd2c6f9504b52e96
Ramsha06 <164333955+Ramsha06@users.noreply.github.com>
2024-06-26 19:28:35 +0500

    Add files via upload

---
+++ 100 days coding challenge/D30/Q88/main.ts
@@ -0,0 +1,11 @@
+// Question 88: Converting Strings to Numbers: Write a function that takes a string representation of a number
+// (e.g., "123") and converts it into an actual number type.
+
+function convertstringtonumbers(num :string):number
+{
+    return Number(num);
+}
+
+console.log(convertstringtonumbers("123"));
+
+
