aac7f28810541297d0404abedc5987141cf4f91f
Ramsha06 <164333955+Ramsha06@users.noreply.github.com>
2024-06-26 19:22:29 +0500

    Add files via upload

---
+++ 100 days coding challenge/D29/Q85/main.ts
@@ -0,0 +1,10 @@
+//Question 85: Finding the Position of a Substring: Write a function that locates the first occurrence of the word
+// "code" within any given string and returns its position.
+
+function findindex (sentence :string):number
+{
+    return (sentence.indexOf("code"));
+
+}
+
+console.log(findindex("Learn to code with JavaScript"));
