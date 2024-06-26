86a5cfddf60db9dd8388e5749701a468e04b6d0f
Ramsha06 <164333955+Ramsha06@users.noreply.github.com>
2024-06-26 19:24:17 +0500

    Add files via upload

---
+++ 100 days coding challenge/D29/Q87/main.ts
@@ -0,0 +1,8 @@
+//Question 87: Extracting a Substring: Demonstrate how to extract the first 10 characters from a string.
+
+function extractingsubstring (str :string):string
+{
+    return str . substring(0,10);
+}
+
+console.log(extractingsubstring("Hello lets learn Javascript"));
