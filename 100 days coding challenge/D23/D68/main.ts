fa2b34e5459195e3b5c2a89d94cc7e2ba8004586
Ramsha06 <164333955+Ramsha06@users.noreply.github.com>
2024-06-26 12:19:26 +0500

    Add files via upload

---
+++ 100 days coding challenge/D23/D68/main.ts
@@ -0,0 +1,14 @@
+//Q68 - Multiplying Decimals: Create a function that accepts two decimal numbers and returns their product. 
+//Round the result to two decimal places.
+
+function multiplyDecimals(num1: number , num2 : number):number
+{
+    // Multiplies the numbers and rounds the result to two decimal places
+    return Math.round(num1 * num2 * 100) / 100;
+  }
+  
+ 
+  console.log(multiplyDecimals(0.1, 0.2));
+{
+
+}
