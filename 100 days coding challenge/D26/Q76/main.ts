7494b4b24a52253d4ea0f02e35ecaecd265853ee
Ramsha06 <164333955+Ramsha06@users.noreply.github.com>
2024-06-26 19:08:55 +0500

    Add files via upload

---
+++ 100 days coding challenge/D26/Q76/main.ts
@@ -0,0 +1,10 @@
+//Question 76: Function Parameters and Return Values: Create a function that takes two numbers as parameters, adds them 
+//together, and returns the result. Show how you can call this function and log the result.
+
+function perametersreturnValue(val1 : number, val2 : number): number
+{
+    return val1 + val2;
+
+}
+
+console.log(perametersreturnValue(5,5));
