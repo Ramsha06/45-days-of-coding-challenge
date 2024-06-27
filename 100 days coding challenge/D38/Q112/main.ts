2c607ea2f8127064fa067b72372e3bf95dcfee20
Ramsha06 <164333955+Ramsha06@users.noreply.github.com>
2024-06-27 15:16:31 +0500

    Add files via upload

---
+++ 100 days coding challenge/D38/Q112/main.ts
@@ -0,0 +1,13 @@
+//Question 112: Create a Map that stores the names of countries as keys and their capitals as values. Add three countries
+// to the Map.
+
+
+    const countries = new Map<string,string>();
+
+    countries.set("Pakistan", "Islamabad");
+    countries.set("USA","Washington , D.C");
+    countries.set("France" , "Paris");
+    countries.set("Japan" , "Tokyo");
+
+    console.log(countries);
+
