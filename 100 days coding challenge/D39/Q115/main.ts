539aa263f61fd77eb74f5605429240e69beeceb2
Ramsha06 <164333955+Ramsha06@users.noreply.github.com>
2024-06-27 20:19:35 +0500

    Add files via upload

---
+++ 100 days coding challenge/D39/Q115/main.ts
@@ -0,0 +1,34 @@
+//Question 115: Use a switch statement to log the days of the week based on a number (1-7).
+
+function DayOfWeek(dayNumber: number): void {
+    switch (dayNumber) {
+      case 1:
+        console.log("Sunday");
+        break;
+      case 2:
+        console.log("Monday");
+        break;
+      case 3:
+        console.log("Tuesday");
+        break;
+      case 4:
+        console.log("Wednesday");
+        break;
+      case 5:
+        console.log("Thursday");
+        break;
+      case 6:
+        console.log("Friday");
+        break;
+      case 7:
+        console.log("Saturday");
+        break;
+      default:
+        console.log("Invalid day number");
+        break;
+    }
+  }
+DayOfWeek(1);
+DayOfWeek(5);
+DayOfWeek(6);
+DayOfWeek(7);
