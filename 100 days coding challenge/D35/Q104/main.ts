990655bd37eaf4ad77d97509cd62198c37e5154b
Ramsha06 <164333955+Ramsha06@users.noreply.github.com>
2024-06-27 15:03:19 +0500

    Add files via upload

---
+++ 100 days coding challenge/D35/Q104/main.ts
@@ -0,0 +1,12 @@
+//Question 104: Create a function that generates a random hexadecimal color code.
+
+function getRandomHexColor(): string {
+  const color =
+    "#" +
+    Math.floor(Math.random() * 0xffffff)
+      .toString(16)
+      .padStart(6, "0");
+  return color; // Returns the random color code
+}
+
+console.log(getRandomHexColor());
