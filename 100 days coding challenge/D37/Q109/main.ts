77d4a63fec3702fc679295295ae4cadcb97239d3
Ramsha06 <164333955+Ramsha06@users.noreply.github.com>
2024-06-27 15:12:32 +0500

    Add files via upload

---
+++ 100 days coding challenge/D37/Q109/main.ts
@@ -0,0 +1,15 @@
+//Question 109: Write an if statement that logs "Good Morning" if the current time is before 12 PM.
+
+ let Time = new Date();
+ if (Time.getHours()<12)
+    {
+        console.log(`Good Morning`);
+    }
+    else 
+    {
+        console.log(`its noon`);
+    
+    };
+
+    
+    
