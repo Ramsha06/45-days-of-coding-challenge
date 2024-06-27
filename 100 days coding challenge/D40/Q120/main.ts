3226eef4a0e63359a9b4ce53f834f15aa67f64ab
Ramsha06 <164333955+Ramsha06@users.noreply.github.com>
2024-06-27 20:27:35 +0500

    Update main.ts

--- 100 days coding challenge/D40/Q120/main.ts
+++ 100 days coding challenge/D40/Q120/main.ts
@@ -1,4 +1,4 @@
-//Question 119: Create a while loop that logs "Hello, World!" 5 times.
+//Question 120: Create a while loop that logs "Hello, World!" 5 times.
 
 let count :number = 0;
 while(count<5)
