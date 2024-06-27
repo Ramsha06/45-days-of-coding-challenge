90a5bc97056ffd0451f9e575a9e0af60092dac93
Ramsha06 <164333955+Ramsha06@users.noreply.github.com>
2024-06-27 20:26:57 +0500

    Add files via upload

---
+++ 100 days coding challenge/D40/Q121/main.ts
@@ -0,0 +1,17 @@
+//Question 120: Use a for...of loop to iterate through an array of your favorite movies and log each one.
+
+let movies : string[]=["KKHH","babies day out", "DDLJ","Hey baby"];
+
+for (let i=0;i<=movies.length; i++)
+    {
+        console.log(movies[i]);
+    }
+
+    //------------------------------------------------------
+
+    const favoritemovies : string[]=["KKHH","babies day out", "DDLJ","Hey baby"];
+
+    for (const movie of favoritemovies)
+        {
+            console.log(movie);
+        }
