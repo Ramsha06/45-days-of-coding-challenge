f4f11094c1ef4baf9ba56addcdc171f99659817f
Ramsha06 <164333955+Ramsha06@users.noreply.github.com>
2024-06-27 14:58:50 +0500

    Add files via upload

---
+++ 100 days coding challenge/D34/Q101/main.ts
@@ -0,0 +1,8 @@
+//Question 101: Generate a random integer between 1 and 10.
+
+function getrandomnumber():number
+{
+    return Math.floor (Math.random()* 10) + 1;
+
+}
+console.log(getrandomnumber());
