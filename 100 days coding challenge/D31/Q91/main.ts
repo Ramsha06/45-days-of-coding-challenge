5214fdd8668fa46095a31394af8dccb65ffb781f
Ramsha06 <164333955+Ramsha06@users.noreply.github.com>
2024-06-27 12:36:59 +0500

    Add files via upload

---
+++ 100 days coding challenge/D31/Q91/main.ts
@@ -0,0 +1,6 @@
+//Question 91: Create an array of your three favorite fruits and add a new fruit to the end of the array.
+
+let fruits : String[]=["Apple" , "Banana" , "Avocado", "Melon"];
+
+let add = fruits.push("Strawberry");
+console.log(fruits);
