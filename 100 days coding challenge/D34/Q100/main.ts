c7f087525e6016f242f8e79c4b74ff9bc0c3ba6e
Ramsha06 <164333955+Ramsha06@users.noreply.github.com>
2024-06-27 14:57:08 +0500

    Add files via upload

---
+++ 100 days coding challenge/D34/Q100/main.ts
@@ -0,0 +1,5 @@
+//Question 100: Use the JavaScript Math object to find the square root of 144.
+
+const sqaureroot : number = Math.sqrt(144);
+
+console.log(`The square root of number 144 = ${sqaureroot}`);
