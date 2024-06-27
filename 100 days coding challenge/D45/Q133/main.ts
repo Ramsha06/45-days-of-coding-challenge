8dea194d021b2c7ffaead9009634ce733994af01
Ramsha06 <164333955+Ramsha06@users.noreply.github.com>
2024-06-27 20:43:20 +0500

    Add files via upload

---
+++ 100 days coding challenge/D45/Q133/main.ts
@@ -0,0 +1,13 @@
+//Question 133: Write a JavaScript object and convert it into a JSON string.
+
+
+const person ={
+    name : "Sifra",
+    age : 25,
+    city : "Saudia",
+
+}
+
+const jsonstring = JSON.stringify(person);
+
+console.log(jsonstring);
